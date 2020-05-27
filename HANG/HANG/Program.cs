74e6b0c7ef562465f63fef23cc79f12056cf8843
Jump3rr <theneonxd@gmail.com>
2020-05-27 13:37:10 +0200

    Wstępne odłączenie logiki od interfejsu. To do list: 1. Naprawić kod aplikacji (po skończeniu rundy nie zaczyna się od początku) 2. Dopisać resztę kodu (instrukcja, informacje) 3. Zaimplementować słownik

---
+++ HANG/HANG/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HANG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj w grze Wisielec!");
+            Gra runda = new Gra("123456789");
+            do
+            {
+                PrintActions();
+                int action = int.Parse(Console.ReadLine());
+                switch (action)
+                {
+                    case 0:
+                        Console.WriteLine("Wyłączanie aplikacji ...");
+                        Environment.Exit(0);
+                        break;
+                    case 1:
+                        runda.Rozgrywka();
+                        break;
+                    /*case 2:
+                        myPhone.AddNewContact(ReadNewContact());
+                        break;*/
+                }
+            }
+            while (true);
+
+        }
+        private static void PrintActions()
+        {
+            Console.WriteLine("Wybierz co chcesz zrobić:");
+            Console.WriteLine("1 - Graj");
+            Console.WriteLine("2 - Zasady gry");
+            Console.WriteLine("3 - Informacje");
+            Console.WriteLine("0 - Wyjście");
+            Console.Write("Wybierz: ");
+        }
+    }
+
+}
